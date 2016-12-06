f78b916badb7cfd807ed78bdcbdecd0306359b17
Gustavo Moraes <gustavosotnas1@gmail.com>
2016-12-06 21:49:17 -0200

    Testa uma implementação para ver os resultados do formulário

--- js/index.js
+++ js/index.js
@@ -1,4 +1,25 @@
 /**
+ * Remove elementos pelo nome da classe.
+ * Não usa jQuery.
+ *
+ * Fonte: http://stackoverflow.com/a/14066534
+ */
+function removeElementsByClass(className){
+  var elements = document.getElementsByClassName(className);
+  while(elements.length > 0){
+    elements[0].parentNode.removeChild(elements[0]);
+  }
+}
+
+/**
+ * Processa as informações, gerando um novo HTML.
+ */
+function verResultados() {
+  removeElementsByClass("form-group");
+  alert("Funfou");
+}
+
+/**
  * Faz formatação automática das caixas de texto para dinheiro e porcentagem.
  * Fonte: https://igorescobar.github.io/jQuery-Mask-Plugin
  */
